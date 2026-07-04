22d78f64e290c494dc66cac7bf566901608e132e
TrueNAS-Script-Kiddie <truenas-master@outlook.com>
2026-07-05 00:31:52 +0200

    bugfix

--- lib/snap_rollup.bash
+++ lib/snap_rollup.bash
@@ -5,6 +5,7 @@
 function Perform_snapshot_rollup() {
     local EXEC_MODE
     local TARGET_POOL
+    local TARGET_APP_POOL
 
     echo "######################################"
     echo "### Performing ZFS snapshot rollup ###"
@@ -13,7 +14,9 @@ function Perform_snapshot_rollup() {
 
     TARGET_POOL="$(Resolve_pool "${LOCAL_TARGET}${REMOTE_TARGET}")"
     [[ -n "${TARGET_POOL}" ]] || Background_error "ERROR: Failed to resolve rollup target pool."
-    ROLLUP_CMD="${SCRIPT_DIR/${LOCAL_SOURCE}${REMOTE_SOURCE}/${LOCAL_TARGET}${REMOTE_TARGET}}/../../zfs-rollup/rollup.py -v --prefix auto -i hourly:48,daily:14,weekly:8,monthly:24,yearly:10 ${TARGET_POOL}/encrypted-ds/media-ds"
+    TARGET_APP_POOL="$(Resolve_pool "${LOCAL_TARGET}${REMOTE_TARGET}" fast)"
+    [[ -n "${TARGET_APP_POOL}" ]] || Background_error "ERROR: Failed to resolve rollup target app pool."
+    ROLLUP_CMD="/mnt/${TARGET_APP_POOL}/encrypted-ds/app-ds/zfs-rollup/rollup.py -v --prefix auto -i hourly:48,daily:14,weekly:8,monthly:24,yearly:10 ${TARGET_POOL}/encrypted-ds/media-ds"
 
     if [[ "${TASK}" == "backup_to_master" && "${LOCAL_SERVER_ID}" == "master" ]] || \
         [[ "${TASK}" == "master_to_backup" && "${LOCAL_SERVER_ID}" == "backup" ]]; then
